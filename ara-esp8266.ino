b3e0a2fd5be109abc604684c24331c4a402e5ded
Ibrahimsyah Zairussalam <ibrahimsyah.zairussalam@tokopedia.com>
2024-03-25 09:08:16 +0700

    feat(pins): add dynamic pins

--- ara-esp8266.ino
+++ ara-esp8266.ino
@@ -52,7 +52,6 @@ void loop() {
         return;
     }
 
-    usecase.readSensor();
     mqttClient.poll();
 }
 
--- usecase.h
+++ usecase.h
@@ -22,117 +22,15 @@ class Usecase {
     // dispatcherMap is map of topic to the executor function that will be
     std::map<String, void (Usecase::*)(DynamicJsonDocument*)> dispatcherMap;
 
-    // actionsMap is map of action type to the executor function that will be
-    // invoked. The json payload will be on form of [[actionType, actionPayload]]
-    std::map<int, void (Usecase::*)(DynamicJsonDocument*)> actionsMap;
+    // actuatorMap is a map of actuator ID to the actuator pin number on the devince.
+    // the ID is used to translate the action from server.
+    std::map<int, int> actuatorMap;
 
     // configurationActionsMap is map of action type to the executor function
     // that will be invoked. The json payload will be on form of [[actionType, configurationCategory, configurationValue]]
     std::map<int, void (Usecase::*)(DynamicJsonDocument*)> configurationActionsMap;
 
-    // doAction will be the entry point of the usecase. It will invoke the
-    // executor function based on the given actionType sent from the server.
-    void doAction(DynamicJsonDocument* json) {
-        int action = (*json)[0].as<int>();
-
-        if (actionsMap.find(action) == actionsMap.end()) {
-            return;
-        }
-
-        (this->*actionsMap[action])(json);
-    }
-
-    // doConfigurationAction will be the entry point of the usecase. It will
-    // invoke the executor function based on the given actionType sent from the
-    // server.
-    void doConfigurationAction(DynamicJsonDocument* json) {
-        int action = (*json)[0].as<int>();
-
-        // If actions is not found, simply fallback to default action
-        // Which is to turn on/off the board LED
-        if (configurationActionsMap.find(action) ==
-            configurationActionsMap.end()) {
-            String state = (*json)[1].as<String>();
-            digitalWrite(LED_BUILTIN, state == "true" ? LOW : HIGH);
-            return;
-        }
-
-        (this->*configurationActionsMap[action])(json);
-    }
-
-    // this function will be invoked when the device is initialized. It will
-    // update the current state of the device based on the given json payload
-    // from the server.
-    void doInitDeviceCurrentState(DynamicJsonDocument* json) {
-        if (json == NULL) {
-            return;
-        }
-
-        if ((*json).as<JsonArray>().size() == 0) {
-            return;
-        }
-
-        for (JsonArray config : (*json).as<JsonArray>()) {
-            int actionType = config[0].as<int>();
-            if (actionType == ACTION_TYPE_BUILT_IN_LED) {
-                String state = config[1].as<String>();
-                digitalWrite(LED_BUILTIN, state == "true" ? LOW : HIGH);
-                continue;
-            }
-
-            if (actionType == ACTION_TYPE_TOGGLE_RELAY) {
-                String state = config[1].as<String>();
-                digitalWrite(PIN_RELAY, state == "true" ? LOW : HIGH);
-                continue;
-            }
-        }
-    }
-
-    void handleToggleBuiltInLED(DynamicJsonDocument* json) {
-        String ledState = (*json)[1].as<String>();
-        digitalWrite(LED_BUILTIN, ledState == "true" ? LOW : HIGH);
-    }
-
-    void handleToggleRelay(DynamicJsonDocument* json) {
-        String relayState = (*json)[1].as<String>();
-        digitalWrite(PIN_RELAY, relayState == "true" ? LOW : HIGH);
-    }
-
-    long readSoilMoistureSensor() {
-        if (!this->isSoilMoistureSensorEnabled) {
-            return -1;
-        }
-
-        if (millis() - this->lastSoilMoistureMillis <
-            SOIL_MOISTURE_INTERVAL_MILLIS) {
-            return -1;
-        }
-
-        this->lastSoilMoistureMillis = millis();
-        int value = analogRead(PIN_SOIL_MOISTURE);
-
-        return value;
-    }
-
-    // updateSoilMoistureSensorConfiguration will update the configuration of
-    // the soil moisture sensor. The json payload will be on form of
-    // [[actionType, configurationCategory, configurationValue]]
-    void updateSoilMoistureSensorConfiguration(DynamicJsonDocument* json) {
-        int configCategory = (*json)[1].as<int>();
-        if (configCategory == CONFIG_SOIL_MOISTURE_ENABLE) {
-            bool isEnabled = (*json)[2].as<bool>();
-            this->isSoilMoistureSensorEnabled = isEnabled;
-            return;
-        }
-
-        if (configCategory == CONFIG_SOIL_MOISTURE_INTERVAL) {
-            int interval = (*json)[2].as<int>();
-            this->SOIL_MOISTURE_INTERVAL_MILLIS = interval;
-            return;
-        }
-    }
-
-   public:
+    public:
     virtual ~Usecase(){};
     Usecase(){};
     Usecase(Repository* repository) : repository(repository) {
@@ -146,20 +44,49 @@ class Usecase {
 
         // Push a request to the server to get the initial state of the device
         // The server will return with another message to update the device
-        this->repository->PushJSON("device-initial-state-request/" DEVICE_ID, NULL);
+        char topic[] = "device-initial-state-request/" DEVICE_ID;
+        this->repository->PushJSON(topic, NULL);
 
         // Define the dispatcher map based on the incoming topic
         this->dispatcherMap["d-" DEVICE_ID] = &Usecase::doAction;
-        this->dispatcherMap["s-" DEVICE_ID] = &Usecase::doConfigurationAction;
         this->dispatcherMap["dcs-" DEVICE_ID] = &Usecase::doInitDeviceCurrentState;
-
-        // Define actions map based on the given first array element of the request
-        this->actionsMap[ACTION_TYPE_BUILT_IN_LED] = &Usecase::handleToggleBuiltInLED;
-        this->actionsMap[ACTION_TYPE_TOGGLE_RELAY] = &Usecase::handleToggleRelay;
-
-        // Define configuration actions map based on the given first array element of the request
-        this->configurationActionsMap[CONFIG_TYPE_SOIL_MOISTURE] = &Usecase::updateSoilMoistureSensorConfiguration;
     };
+
+    // doAction will be the entry point of the usecase. It will invoke the
+    // executor function based on the given actionType sent from the server.
+    void doAction(DynamicJsonDocument* json) {
+        int actuatorID = (*json)[0].as<int>();
+        if (actuatorMap.find(actuatorID) == actuatorMap.end()) {
+            return;
+        }
+
+        String value = (*json)[1].as<String>();
+        digitalWrite(this->actuatorMap[actuatorID], value == "true" ? LOW : HIGH);
+
+    }
+
+    // this function will be invoked when the device is initialized. It will
+    // update the current state of the device based on the given json payload
+    // from the server.
+    void doInitDeviceCurrentState(DynamicJsonDocument* json) {
+        if (json == NULL) {
+            return;
+        }
+
+        // Register available actuator
+        JsonArray actuators = (*json)["a"].as<JsonArray>();
+        for (JsonArray item : actuators) {
+            int pinNumber = item[1];
+            actuatorMap[pinNumber] = item[0];
+            pinMode(pinNumber, OUTPUT);
+        }
+
+        // Initialize actuator state
+        JsonArray states = (*json)["v"].as<JsonArray>();
+        for (JsonArray config : states) {
+            digitalWrite(this->actuatorMap[config[0]], config[1]);
+        }
+    }
 
     void dispatchMQMessage(String topic, DynamicJsonDocument* json) {
         if (dispatcherMap.find(topic) == dispatcherMap.end()) {
@@ -168,23 +95,6 @@ class Usecase {
 
         (this->*dispatcherMap[topic])(json);
     }
-
-    void readSensor() {
-        DynamicJsonDocument doc(1024);
-        int currentIndex = -1;
-        long soilMoisture = readSoilMoistureSensor();
-        if (soilMoisture != -1) {
-            currentIndex++;
-            doc[currentIndex][0] = 3;
-            doc[currentIndex][1] = soilMoisture;
-        }
-
-        if (currentIndex < 0) {
-            return;
-        }
-
-        this->repository->PushJSON("sensor-read/" DEVICE_ID, &doc);
-    };
 };
 
 #endif
